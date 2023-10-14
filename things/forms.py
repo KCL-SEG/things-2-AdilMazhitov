de192bb4120025bd53d874420356979bdbbbeb61
Adil Mazhitov <k21117945@kcl.ac.uk>
2023-10-15 01:24:59 +0600

    Display the form in the home view

--- things/forms.py
+++ things/forms.py
@@ -5,11 +5,11 @@ from django import forms
 from .models import Thing
 
 class ThingForm(forms.ModelForm):
-    name = forms.CharField(label = 'Name', max_length = 30)
     class Meta:
         model = Thing
-        fields =['description','quantity']
-        widgets = {'description': forms.Textarea(),
+        fields =['name','description','quantity']
+        widgets = {'name': forms.CharField(),
+                    'description': forms.Textarea(),
                     'quantity': forms.NumberInput(
                         attrs= {
                             'max':'50',
